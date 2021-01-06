b6663dc4019465ef3731ef6ff04a3a551b7c9810
Nguyen Huu Phuc <huuphuc.nguyen@nfq.asia>
2021-01-06 14:37:03 +0700

    update script test

--- docker-test.sh
+++ docker-test.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+if which node > /dev/null
+    then
+        NODE_INSTALLED=1
+        echo "NPM installed"
+    else
+        NODE_INSTALLED=0
+        echo "NPM NOT installed"
+    fi
+
+
 echo "INIT ENV"
 cp .env.dist .env.test.local
 docker-compose exec php bash -c "sed -i 's/APP_ENV=dev/APP_ENV=test/' .env.test.local"
 docker-compose exec php bash -c "sed -i 's/MYSQL_URL=mysql:\/\/dev:dev@mysql:3306\/dev/MYSQL_URL=mysql:\/\/dev:dev@mysql:3306\/test/' .env.test.local"
 docker-compose exec php bash -c "sed -i 's/DATABASE_DBNAME=logs/DATABASE_DBNAME=default/' .env.test.local"
-docker-compose exec php bash -c "sed -i 's/APP_WEBPACK_FOLDER=assets/APP_WEBPACK_FOLDER=build/' .env.test.local"
+if [ "$NODE_INSTALLED" == "1" ]; then
+    docker-compose exec php bash -c "sed -i 's/APP_WEBPACK_FOLDER=assets/APP_WEBPACK_FOLDER=build/' .env.test.local"
+fi
 
 echo "CREATE test database"
 docker-compose exec mysql bash -c "mysql -udev -pdev -e \"CREATE DATABASE IF NOT EXISTS test;\""
@@ -19,9 +31,19 @@ docker-compose exec php bash -c "php bin/console cache:clear --env=\"test\""
 echo "MIGRATION"
 docker-compose exec php bash -c "php bin/console doctrine:migrations:migrate --no-interaction --env=\"test\""
 
-echo "NPM"
-npm install
-npm run dev
+if [ "$NODE_INSTALLED" == "1" ]; then
+    echo "NPM"
+    if test -f .env; then
+        mv .env .env.npm.bak
+        cp .env.test.local .env
+    fi
+    npm install
+    npm run dev
+    if test -f .env.npm.bak; then
+        rm -rf .env
+        mv .env.npm.bak .env
+    fi
+fi
 
 echo "FIXTURES"
 docker-compose exec php bash -c "php bin/console doctrine:fixtures:load -n --env=\"test\""
