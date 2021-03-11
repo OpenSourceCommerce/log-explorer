11c25d85dbfe982198d1e8d21fb8300cad3f1c84
Nguyen Huu Phuc <huuphuc.nguyen@nfq.asia>
2021-03-11 11:08:27 +0700

    update for create dashboard

--- src/Form/DashboardType.php
+++ src/Form/DashboardType.php
@@ -4,9 +4,9 @@ namespace App\Form;
 
 use App\Entity\Dashboard;
 use App\Services\Widget\WidgetServiceInterface;
+use App\Validator\DashboardWidgetSameTable;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -40,9 +40,9 @@ class DashboardType extends AbstractType
             ->add('widgets', ChoiceType::class, [
                 'mapped' => false,
                 'choices' => $this->getWidgetIds(),
-                'multiple' => false,
+                'multiple' => true,
                 'constraints' => [
-                    new DashboardWidgetIds(),
+                    new DashboardWidgetSameTable(),
                 ],
             ])
         ;
