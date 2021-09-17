8b5ada4f7368e13671c4de1b6648bf82b9311e10
Nguyen Huu Phuc <huuphuc.nguyen@nfq.asia>
2021-09-17 15:17:16 +0700

    refactor for large exports

--- src/Services/Export/ExportService.php
+++ src/Services/Export/ExportService.php
@@ -4,8 +4,9 @@ namespace App\Services\Export;
 
 use App\Constant\ExportConstant;
 use App\Entity\Export;
-use App\Entity\User;
 use App\Repository\ExportRepository;
+use App\Services\Stream\StreamServiceInterface;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Filesystem\Filesystem;
@@ -24,13 +25,22 @@ class ExportService implements ExportServiceInterface
      * @var Filesystem
      */
     private $filesystem;
+    /**
+     * @var StreamServiceInterface
+     */
+    private $streamService;
 
-    public function __construct(EntityManagerInterface $em, ParameterBagInterface $parameterBag, Filesystem $filesystem)
-    {
+    public function __construct(
+        EntityManagerInterface $em,
+        ParameterBagInterface $parameterBag,
+        Filesystem $filesystem,
+        StreamServiceInterface $streamService
+    ) {
 
         $this->em = $em;
         $this->parameterBag = $parameterBag;
         $this->filesystem = $filesystem;
+        $this->streamService = $streamService;
     }
 
     /**
@@ -68,19 +78,106 @@ class ExportService implements ExportServiceInterface
         return $this->getRepository()->findBy(['finishedAt' => null], ['id' => 'asc'], $limit);
     }
 
+    private function slitFilter(array $filter): array
+    {
+        $filters = [];
+        /** @var DateTime $from */
+        $from = $filter['from'];
+        /** @var DateTime $to */
+        $to = $filter['to'];
+        $interval = $this->parameterBag->get('app.logview.export_max_range');
+        $range = $interval * 3600 - 1;
+        while (true) {
+            $option = $filter;
+            $diff = $from->diff($to);
+            $diffHour = $diff->days * 24 + $diff->h;
+            $option['from'] = clone $from;
+            if ($diffHour > $interval) {
+                $newTo = clone $from;
+                $newTo->add(new \DateInterval("PT{$range}S"));
+                $option['to'] = $newTo;
+                $from->add(new \DateInterval("PT{$interval}H"));
+                $filters[] = $option;
+            } else {
+                $filters[] = $option;
+                break;
+            }
+        }
+        return $filters;
+    }
+
+    private function makeFilter(array $filter): array
+    {
+        $options = [
+            'limit' => 0
+        ];
+
+        if (!empty($from = $filter['from'])) {
+            if (is_numeric($from)) {
+                $from = new DateTime("- {$from} minutes");
+            } else {
+                $from = new DateTime($from);
+            }
+            $options['from'] = $from;
+        } else {
+            $options['from'] = new DateTime('- 1 hour');
+        }
+
+        if (!empty($filter['to'])) {
+            $to = new DateTime($filter['to']);
+            $options['to'] = $to;
+        } else {
+            $options['to'] = new DateTime();
+        }
+
+        if (!empty($filter['filter'])) {
+            $options['filter'] = $filter['filter'];
+        } else {
+            $options['filter'] = false;
+        }
+
+        return $this->slitFilter($options);
+    }
+
+    /**
+     * @param Export $export
+     * @return string
+     */
+    private function generateExportFilename(Export $export): string
+    {
+        $date = new DateTime();
+        return uniqid("{$export->getTable()}_{$date->format('Y-m-d_H-i-s')}-") . "." . $export->getFormat();
+    }
+
     /**
      * @inheritDoc
      */
-    public function export(string $filename, string $format, array $data): ?string
+    public function export(Export $export): ?string
     {
-        switch ($format) {
-            case ExportConstant::CSV_FORMAT:
-                return $this->exportCsv($filename, $data);
-            case ExportConstant::JSON_FORMAT:
-                return $this->exportJson($filename, $data);
+        $filters = $this->makeFilter($export->getFilter());
+        $filename = $this->generateExportFilename($export);
+        $subDirectory = $this->generateSubDirectory($filename);
+
+        $total = count($filters) - 1;
+
+        foreach ($filters as $k => $filter) {
+            $data = $this->streamService->getLogsInRange($export->getTable(), $filter);
+            $mode = empty($k) ? self::MODE_NEW_FILE : self::MODE_APPEND_FILE;
+            $isEnd = $total == $k;
+            if ($mode != self::MODE_NEW_FILE && empty($data) && !$isEnd) {
+                continue;
+            }
+            switch ($export->getFormat()) {
+                case ExportConstant::CSV_FORMAT:
+                    $this->exportCsv($subDirectory, $filename, $data, $mode);
+                    break;
+                case ExportConstant::JSON_FORMAT:
+                    $this->exportJson($subDirectory, $filename, $data, $mode, $isEnd);
+                    break;
+            }
         }
 
-        return null;
+        return $this->getOutputPath().'/'.$subDirectory.'/'.$filename;
     }
 
     private function getOutputPath(): string
@@ -104,35 +201,56 @@ class ExportService implements ExportServiceInterface
     /**
      * @inheritDoc
      */
-    public function exportCsv(string $filename, array $data): ?string
+    public function exportCsv(string $subDirectory, string $filename, array $data, int $mode = self::MODE_SINGLE_FILE)
     {
-        $subDirectory = $this->generateSubDirectory($filename);
         $fullPath = "{$this->getOutputDirectory($subDirectory)}/{$filename}";
-        $this->deleteExistingFile($fullPath);
-        $file = fopen($fullPath, 'w');
 
-        fputcsv($file, array_keys($data[0]));
+        if ($mode == self::MODE_SINGLE_FILE || $mode == self::MODE_NEW_FILE) {
+            $this->deleteExistingFile($fullPath);
+            $file = fopen($fullPath, 'w');
+            fputcsv($file, array_keys($data[0]));
+        } else {
+            $file = fopen($fullPath, 'a');
+        }
 
         foreach ($data as $row) {
             fputcsv($file, $row);
         }
 
-        return "{$this->getOutputPath()}/{$subDirectory}/{$filename}";
+        fclose($file);
     }
 
     /**
      * @inheritDoc
      */
-    public function exportJson(string $filename, array $data): ?string
+    public function exportJson(string $subDirectory, string $filename, array $data, int $mode = self::MODE_SINGLE_FILE, bool $isEnd = false)
     {
-        $subDirectory = $this->generateSubDirectory($filename);
         $fullPath = "{$this->getOutputDirectory($subDirectory)}/{$filename}";
-        $this->deleteExistingFile($fullPath);
-        $file = fopen($fullPath, 'w');
 
-        fputs($file, json_encode($data));
+        if ($mode == self::MODE_NEW_FILE || $mode == self::MODE_SINGLE_FILE) {
+            $this->deleteExistingFile($fullPath);
+            $file = fopen($fullPath, 'w');
+        } else {
+            $file = fopen($fullPath, 'a');
+        }
 
-        return "{$this->getOutputPath()}/{$subDirectory}/{$filename}";
+        if ($mode == self::MODE_SINGLE_FILE) {
+            fputs($file, json_encode($data));
+        } elseif ($mode == self::MODE_NEW_FILE) {
+            if ($isEnd) {
+                fputs($file, json_encode($data));
+            } else {
+                fputs($file, rtrim(json_encode($data), "]"));
+            }
+        } else {
+            if ($isEnd) {
+                fputs($file, (empty($data) ? '' : ',') . ltrim(json_encode($data), "["));
+            } else {
+                fputs($file, ',' . trim(json_encode($data), "[]"));
+            }
+        }
+
+        fclose($file);
     }
 
     private function deleteExistingFile($fullPath)
