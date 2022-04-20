d4a4114e53b945bfebe60be3eb66a52554e3d774
datnguyen <dat.nguyen@nfq.asia>
2022-04-20 11:40:35 +0700

    LOG-16
    LOG-16 Forgot Password Page

--- assets/js/pages/security/_forgot-form.js
+++ assets/js/pages/security/_forgot-form.js
@@ -1,84 +1,144 @@
-import React, {Component} from 'react';
-import {UserActions} from '../../actions';
-import {Input, Button, Colors} from '../../components';
+import React, { Component } from "react";
+import { UserActions } from "../../actions";
+import { Input, Button, Colors, Image } from "../../components";
+import Logo from "../../../images/light-logo.png";
 
+import EmailSent from "./email-sent.json";
+import { Player } from "@lottiefiles/react-lottie-player";
 export class ForgotForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: '',
-            emailError: ''
+            email: "",
+            emailError: "",
+            emailSentSuccess: false,
         };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(e) {
-        if (e.target.name === 'email') {
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        if (name === "email") {
             this.setState({
-                email: e.target.value
+                email: value,
             });
         }
-    }
+    };
 
-    handleSubmit(e) {
+    handleSubmit = (e) => {
         e.preventDefault();
 
-        UserActions.forgot(this.state.email).then(response => {
+        UserActions.forgot(this.state.email).then((response) => {
             if (response.error === 0) {
                 this.reset();
                 this.setState({
-                    message: {
-                        type: 'success',
-                        message: 'We have sent you an email to reset your password. Please follow the instruction to reset your password!'
-                    }
+                    emailSentSuccess: true,
                 });
             } else {
                 this.setState({
                     message: {
-                        type: 'danger',
-                        message: response.fields.email
-                    }
+                        type: "danger",
+                        message: response.fields.email,
+                    },
                 });
             }
         });
+    };
+
+    reset = () => {
+        this.setState({
+            email: "",
+        });
+        $("#email").focus();
     }
 
-    reset() {
-        this.setState({
-            email: '',
-        });
-        $('#email').focus();
-    }
+    returnLogin = () => {
+        window.location.href = "/";
+    };
 
     render() {
-        const {message, email} = this.state
+        const { message, email, emailSentSuccess } = this.state;
+
+        const EmailSentSuccess = () => (
+            <div className="card-body text-center">
+                <Player
+                    src={EmailSent}
+                    background="transparent"
+                    speed="0.7"
+                    style={{ width: "197px", height: "197px" }}
+                    autoplay
+                />
+                <h4>We sent you an email</h4>
+
+                <label className="w-75 mb-3">
+                    Check your inbox for our mail and follow the steps described to retrieve your
+                    password.
+                </label>
+            </div>
+        );
 
         return (
-            <form onSubmit={this.handleSubmit}>
-                {message &&
-                <div className={`alert alert-${message.type}`}>
-                    <div className="alert-message">
-                        {message.message}
-                    </div>
-                </div>
-                }
-                <div className="form-group">
-                    <Input type="email"
-                        id="email"
-                        name="email"
-                        value={email}
-                        onChange={this.handleChange}
-                        required="required"
-                        autoFocus={true}
-                        placeholder={'Enter your email'}/>
-                </div>
-                <div className="mt-3">
-                    <Button type="submit" className={'btn-block'}
-                        color={Colors.blue}>{'Reset'}</Button>
-                </div>
-            </form>
+            <div className="reset-form card">
+                {emailSentSuccess ? (
+                    <>
+                        <div className="card-header bg-white">
+                            <a
+                                href="/"
+                                className=" d-flex flex-column w-100 justify-content-center align-items-center"
+                            >
+                                <Image
+                                    src={Logo}
+                                    className="login-logo w-50 mt-4 mb-4"
+                                    alt="ScaleCommerce · E-Commerce"
+                                />
+                            </a>
+                        </div>
+                        <div className="card-body text-center">
+                            <label className="w-75">
+                                You forgot your password? Here you can easily retrieve a new
+                                password.
+                            </label>
+                            <form className="ms-3 me-3" onSubmit={this.handleSubmit}>
+                                {message && (
+                                    <div className={`alert alert-${message.type}`}>
+                                        <div className="alert-message">{message.message}</div>
+                                    </div>
+                                )}
+                                <div className="form-group text-start">
+                                    <label htmlFor="email" className="form-label">
+                                        Email
+                                    </label>
+                                    <Input
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        value={email}
+                                        onChange={this.handleChange}
+                                        required="required"
+                                        autoFocus={true}
+                                        placeholder={"Input your login email"}
+                                    />
+                                </div>
+                                <Button
+                                    type="submit"
+                                    className="btn-block w-100 mt-3"
+                                    color={Colors.blue}
+                                >
+                                    Reset Password
+                                </Button>
+                                <button
+                                    className="btn text-primary w-100 mt-3"
+                                    role="link"
+                                    onClick={this.returnLogin}
+                                >
+                                    Return to Login
+                                </button>
+                            </form>
+                        </div>
+                    </>
+                ) : (
+                    <EmailSentSuccess />
+                )}
+            </div>
         );
     }
 }
-
