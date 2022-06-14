c7a47add3fc2785ed6a925ce12fe1ecdf0ac4856
datnguyen <dat.nguyen@nfq.asia>
2022-06-14 11:37:24 +0700

    Rename file

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -26,15 +26,7 @@ class DashboardController extends AbstractController
         if ($dashboard) {
             return $this->redirectToRoute('dashboard', ['uuid' => $dashboard->getUuid()]);
         }
-        return $this->render('dashboard/list.html.twig');
-    }
-
-    /**
-     * @Route("/dashboard/list", priority=10, name="dashboard_list")
-     */
-    public function list(): Response
-    {
-        return $this->render('dashboard/list.html.twig');
+        return $this->render('dashboard/empty.html.twig');
     }
 
     /**
