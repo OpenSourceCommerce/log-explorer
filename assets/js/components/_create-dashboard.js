a3e2002e7c5160ab38b2f9ead83cce1b366e2d4c
datnguyen <dat.nguyen@nfq.asia>
2022-06-23 11:40:57 +0700

    Fix component not import yet

--- assets/js/components/_create-dashboard.js
+++ assets/js/components/_create-dashboard.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { AlertMessage, Button, FormField, Modal, Size } from ".";
+import React, { useState } from "react";
+import { AlertMessage, Button, Colors, FormField, Modal, Size } from ".";
+import { DashboardActions } from "../actions";
 
 export const CreateNewDashboardModal = ({ isShow, onHidden }) => {
     const [dashboardName, setDashboardName] = useState("");
