012dc9a0c110f27238dc3667d750359674d3502d
duyvodev <duy.vo@nfq.asia>
2022-08-30 09:20:41 +0700

    LOG-89: fix stick widget

--- assets/js/components/widget/_widget-header.js
+++ assets/js/components/widget/_widget-header.js
@@ -1,41 +1,42 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {Icon} from "../_icon";
+import React from "react";
+import PropTypes from "prop-types";
+import { Icon } from "../_icon";
 
-export class WidgetHeader extends Component {
-    render() {
-        const {
-            header,
-            removeWidget,
-            editWidget,
-            stickWidget,
-            isFixed,
-        } = this.props;
-        return (
-            <div className="card-header pb-0 pt-2 ps-3 pe-2 fw-bold border-bottom-0 bg-white">
-                <div className="float-start">{`${header || '< Input Header >'}`}</div>
-                {isUser() || <div className="dropdown float-end"
-                     href="#"
-                >
-                    <div className="pe-2 ps-2"
-                         data-bs-toggle="dropdown"
-                         aria-haspopup="true"
-                         aria-expanded="false"
+export function WidgetHeader(props) {
+    const { header, removeWidget, editWidget, stickWidget, isFixed } = props;
+
+    return (
+        <div className="card-header pb-0 pt-2 ps-3 pe-2 fw-bold border-bottom-0 bg-white">
+            <div className="float-start">{`${header || "< Input Header >"}`}</div>
+            {isUser() || (
+                <div className="dropdown float-end" href="#">
+                    <div
+                        className="pe-2 ps-2"
+                        data-bs-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
                     >
-                        <Icon name='ellipsis-h'/>
+                        <Icon name="ellipsis-h" />
                     </div>
-                    <div className="dropdown-menu dropdown-menu-end"
-                         aria-labelledby="dropdownMenuButton"
-                         style={{zIndex:"1050"}}
+                    <div
+                        className="dropdown-menu dropdown-menu-end"
+                        aria-labelledby="dropdownMenuButton"
+                        style={{ zIndex: "1050" }}
                     >
-                        <a className="dropdown-item" href="#" onClick={() => stickWidget(!isFixed)}>{isFixed ? 'Unstick widget' : 'Stick widget'}</a>
-                        <a className="dropdown-item" href="#" onClick={() => editWidget()}>Edit</a>
-                        <a className="dropdown-item" href="#" onClick={() => removeWidget()}>Remove</a>
+                        <a className="dropdown-item" href="#" onClick={() => stickWidget(!isFixed)}>
+                            {isFixed ? "Unstick widget" : "Stick widget"}
+                        </a>
+                        <a className="dropdown-item" href="#" onClick={() => editWidget()}>
+                            Edit
+                        </a>
+                        <a className="dropdown-item" href="#" onClick={() => removeWidget()}>
+                            Remove
+                        </a>
                     </div>
-                </div>}
-            </div>
-        );
-    }
+                </div>
+            )}
+        </div>
+    );
 }
 
 WidgetHeader.propTypes = {
